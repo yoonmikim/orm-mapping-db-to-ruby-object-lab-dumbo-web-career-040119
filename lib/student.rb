71d288f9ecde9ac19fdadca46ee44e59dd98f269
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-10 19:57:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,10 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row) }
   end
   
-  def first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students 
       WHERE grade = 10
-      ORDER BY id
       LIMIT ?
     SQL
     DB[:conn].execute(sql,number).map{|row|self.new_from_db(row)}
